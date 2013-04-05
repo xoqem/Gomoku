de52381025d5c7d006e9515fcf1b4f19fd0624bc
xoqem <sinfree@gmail.com>
2013-04-05 01:23:04 -0700

    uses 1D array for binding purposes

--- js/controllers/board_controller.js
+++ js/controllers/board_controller.js
@@ -1,7 +1,12 @@
 App.boardController = Ember.ArrayController.extend({
-  content: [[0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]],
+  content: [0, 0, 0,
+            0, 0, 0,
+            0, 0, 0],
+
+  getIndex: function(x, y) {
+    this.checkBounds(x, y);
+    return x + y * 3;
+  },
 
   checkBounds: function(x, y) {
     if (x < 0 || x > 2 || y < 0 || y > 2) {
@@ -16,25 +21,22 @@ App.boardController = Ember.ArrayController.extend({
   },
 
   getCell: function(x, y) {
-    this.checkBounds(x, y);
-    return this.get('content')[x][y];
+    return this.get('content')[this.getIndex(x, y)];
   },
 
   setCell: function(x, y, value) {
-    this.checkBounds(x, y);
     var numValue = Number(value);
     this.checkValue(numValue);
-    this.get('content')[x][y] = numValue;
-    // TODO: do we need to manually dispatch content changed event, since we
-    // are updating the array manually?
+    this.replaceContent(this.getIndex(x, y), 1, [numValue]);
   },
 
   reset: function() {
-    for (var i = 0; i < 3; i++) {
-      for (var j = 0; j < 3; j++) {
-        this.setCell(i, j, 0);
-      }
+    var i = this.get('content').length;
+    var array = [];
+    while (i--) {
+      array.push(0);
     }
+    this.set('content', array);
   },
 
   textOutput: function() {
