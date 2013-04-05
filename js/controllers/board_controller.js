20f77f2d55ff2dcbfa9a2eb7ea2816012ebf492b
xoqem <sinfree@gmail.com>
2013-04-05 13:35:32 -0700

    checks for winner, dynamic board size

--- js/controllers/board_controller.js
+++ js/controllers/board_controller.js
@@ -1,10 +1,12 @@
 App.boardController = Ember.ArrayController.extend({
   content: [],
+  winner: 0,
+  size: 3,
 
   initialize: function() {
     var array = [];
-    for (var y = 0; y < 3; y++) {
-      for (var x = 0; x < 3; x++) {
+    for (var y = 0; y < this.get('size'); y++) {
+      for (var x = 0; x < this.get('size'); x++) {
         array.push(
           App.Cell.create({
             x: x,
@@ -15,15 +17,16 @@ App.boardController = Ember.ArrayController.extend({
       }
     }
     this.set('content', array);
+    this.set('winner', 0);
   },
 
   getIndex: function(x, y) {
     this.checkBounds(x, y);
-    return x + y * 3;
+    return x + y * this.get('size');
   },
 
   checkBounds: function(x, y) {
-    if (x < 0 || x > 2 || y < 0 || y > 2) {
+    if (x < 0 || x > (this.get('size') - 1) || y < 0 || y > (this.get('size') - 1)) {
       throw new Error("checkBounds: " + x + ", " + y + " out of bounds");
     }
   },
@@ -34,11 +37,48 @@ App.boardController = Ember.ArrayController.extend({
     }
   },
 
+  checkWinner: function() {
+    var i;
+    var j;
+    var diagWinner1 = this.getCell(0, 0).value;
+    var diagWinner2 = this.getCell(0, this.get('size') - 1).value;
+    for (i = 0; i < this.get('size'); i++) {
+      if (this.getCell(i, i).value !== diagWinner1) diagWinner1 = 0;
+      if (this.getCell(i, this.get('size') - 1 - i).value !== diagWinner2) diagWinner2 = 0;
+
+      var horizontalWinner = this.getCell(0, i).value;
+      var verticalWinner = this.getCell(i, 0).value;
+      for (j = 1; j < this.get('size'); j++) {
+        if (this.getCell(j, i).value !== horizontalWinner) horizontalWinner = 0;
+        if (this.getCell(i, j).value !== verticalWinner) verticalWinner = 0;
+      }
+      if (horizontalWinner !== 0) {
+        this.set('winner', horizontalWinner);
+        return;
+      }
+      if (verticalWinner !== 0) {
+        this.set('winner', verticalWinner);
+        return;
+      }
+    }
+
+    if (diagWinner1 !== 0) {
+      this.set('winner', diagWinner1);
+      return;
+    }
+    if (diagWinner2 !== 0) {
+      this.set('winner', diagWinner2);
+      return;
+    }
+  },
+
   getCell: function(x, y) {
     return this.get('content')[this.getIndex(x, y)];
   },
 
   setCellValue: function(x, y, value) {
+    if (this.getCell(x, y).value !== 0) return;
+
     var numValue = Number(value);
     this.checkValue(numValue);
     this.replaceContent(this.getIndex(x, y), 1, [
@@ -48,12 +88,15 @@ App.boardController = Ember.ArrayController.extend({
         value: numValue
       })
     ]);
+
+    this.checkWinner();
+    console.log('winner:', this.get('winner'));
   },
 
   textOutput: function() {
     var output = "";
-    for (var i = 0; i < 3; i++) {
-      for (var j = 0; j < 3; j++) {
+    for (var i = 0; i < this.get('size'); i++) {
+      for (var j = 0; j < this.get('size'); j++) {
         output += this.getCell(i, j) + " ";
       }
       output += "\n";
--- js/utils/handlebars_utils.js
+++ js/utils/handlebars_utils.js
@@ -1,21 +1,17 @@
 App.HandlebarsUtil = Ember.Object.createWithMixins({
   registerHelpers: function(str, data) {
 
-    Ember.Handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
-      if (arguments.length < 3) {
-        throw new Error("Handlebars Helper equal needs 2 parameters");
-      }
-
-      var propValue = Ember.Handlebars.get(this, lvalue, options);
-      if(propValue != rvalue) {
+    Ember.Handlebars.registerHelper('shouldWrap', function(value, options) {
+      var propValue = Ember.Handlebars.get(this, value, options);
+      if(propValue != (App.boardController.size - 1)) {
         return options.inverse(this);
       } else {
         return options.fn(this);
       }
     });
 
-    Ember.Handlebars.registerHelper('cellValue', function(value) {
-      switch (Ember.Handlebars.get(this, value)) {
+    Ember.Handlebars.registerBoundHelper('cellValue', function(value) {
+      switch (value) {
         case 1:
           return "X";
 
