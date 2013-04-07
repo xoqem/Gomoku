7b4ae404e97467f48ee587d4d0fe167bc5ec77ac
xoqem <sinfree@gmail.com>
2013-04-07 16:00:59 -0700

    more aggressive AI

--- js/models/possible_move.js
+++ js/models/possible_move.js
@@ -7,6 +7,10 @@ App.PossibleMove = Ember.Object.extend({
 
 App.PossibleMove.reopen({
   getScore: function() {
-    return this.get('runs') + this.get('blocks');
+    // TODO making the AI prefer runs over blocks makes it slightly more
+    // aggressive (which seemed to play a better game), but it would be
+    // nice to be able to tweak how much it waits each of these to make
+    // it more or less agressive
+    return (this.get('runs') * 2) + this.get('blocks');
   }
 });
