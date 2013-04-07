acf413c77009ed9cb924cad035cbdbba46c02319
xoqem <sinfree@gmail.com>
2013-04-06 19:32:41 -0700

    moves player logic to players controller

--- js/controllers/game_controller.js
+++ js/controllers/game_controller.js
@@ -1,46 +1,22 @@
 App.gameController = Ember.Controller.extend({
-  currentPlayer: null,
   gameOver: false,
-  players: [],
   winner: null,
 
   initialize: function() {
+    App.playersController.initialize();
     App.boardController.initialize();
-
-    // create the players
-    this.set('players', [
-      App.Player.create({
-        icon: "images/cell-blue.png",
-        name: "blue",
-        isHuman: true
-      }),
-      App.Player.create({
-        icon: "images/cell-red.png",
-        name: "red",
-        isHuman: false
-      })
-    ]);
-
     this.reset();
   },
 
   playTurn: function(point) {
     if (this.get('gameOver') === true) return;
 
-    var player = this.get('currentPlayer');
-    var playersArray = this.get('players');
     // if able to play in spot, switch to next player and check winner
-    if (App.boardController.setCellPlayer(point, player)) {
-      // select next player in players array
-      var nextPlayer;
-      for (var i = 1; i < playersArray.length; i++) {
-        if (playersArray[i - 1] == player) {
-          nextPlayer = playersArray[i];
-          break;
-        }
-      }
-      nextPlayer = nextPlayer || playersArray[0];
-      this.set('currentPlayer', nextPlayer);
+    if (App.boardController.setCellPlayer(point, App.playersController.get('player'))) {
+      // shift current player to the next player, even if this turn is the end
+      // of the current game, we want the current player to be set correctly
+      // for the next turn or next game
+      App.playersController.nextPlayer();
 
       // check winner
       var winningPlayer = App.boardController.getWinner();
@@ -48,10 +24,17 @@ App.gameController = Ember.Controller.extend({
       if (winningPlayer !== null || App.boardController.get('openCells') === 0) {
         this.set('gameOver', true);
       }
-      else if (nextPlayer.isHuman === false) {
-        // if computer player, do the AI move
-        App.aiController.playNextMove(nextPlayer);
+      else {
+        this.startNextTurn();
       }
+    }
+  },
+
+  startNextTurn: function() {
+    var player = App.playersController.get('player');
+    // if computer player, do the AI move
+    if (player.isHuman === false) {
+      App.aiController.playNextMove(player);
     }
   },
 
@@ -59,16 +42,6 @@ App.gameController = Ember.Controller.extend({
     App.boardController.reset();
     this.set('winner', null);
     this.set('gameOver', false);
-
-    // If we have a player from last game, then this is who should start, since
-    // the game increments to the next player at the end of each turn.  If we
-    // don't have a player yet, just use the first one in the list.
-    var player = this.get('currentPlayer') || this.get('players')[0];
-    this.set('currentPlayer', player);
-
-    if (player.isHuman === false) {
-        // if computer player, do the AI move
-        App.aiController.playNextMove(player);
-      }
+    this.startNextTurn();
   }
 }).create();
---
+++ js/controllers/players_controller.js
@@ -0,0 +1,35 @@
+App.playersController = Ember.ArrayController.extend({
+  content: null,
+  playerIndex: NaN,
+  player: function() {
+    var index = this.get('playerIndex');
+    if (isNaN(index)) return null;
+    return this.objectAt(index);
+  }.property('playerIndex'),
+
+  initialize: function() {
+    // create the players
+    var players = [
+      App.Player.create({
+        icon: "images/cell-blue.png",
+        name: "blue",
+        isHuman: true
+      }),
+      App.Player.create({
+        icon: "images/cell-red.png",
+        name: "red",
+        isHuman: false
+      })
+    ];
+
+    this.set('content', players);
+    this.set('playerIndex', 0);
+  },
+
+  nextPlayer: function() {
+    var index = this.get('playerIndex');
+    index++;
+    if (index >= this.get('length')) index = 0;
+    this.set('playerIndex', index);
+  }
+}).create();
