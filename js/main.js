073fba13e42b9dca64d7f3d7aec63bec6722c508
xoqem <sinfree@gmail.com>
2014-04-26 01:36:27 -0700

    Update dependency versions

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ require.config({
   paths: {
     templates : '../templates',
     text: '../libs/text.min',
-    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min',
-    handlebars: '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.2.1/handlebars.min',
-    ember: '//cdnjs.cloudflare.com/ajax/libs/ember.js/1.2.0/ember.min'
+    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1-rc2/jquery.min',
+    handlebars: '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.3.0/handlebars.min',
+    ember: '//cdnjs.cloudflare.com/ajax/libs/ember.js/1.5.1/ember.min'
   },
   shim: {
     'ember': {
